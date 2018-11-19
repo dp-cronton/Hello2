56e16aabe0d32a7cec924777c54065ead4aec70e
David Petticrew <David.Petticrew@riversidecollege.ac.uk>
2018-11-19 15:21:47 +0000

    introduced bugs

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            Message8ox.Show("Hello"):
         }
     }
 }
